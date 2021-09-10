32eed9259f9cf032d9c63c578bc7809ad8f66cba
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2021-09-11 00:44:16 +0300

    works + animation

--- public/script/query.js
+++ public/script/query.js
@@ -1,28 +1,2 @@
 $('.intro').hide().slideDown(800)
 
-$(window).scroll
-$(window).scrollTop(function () {
-    if ($(this).scrollTop() >= 1000) {
-        // длительность анимации - 'slow'
-        // тип анимации -  'linear'
-        $('.intro').slideUp('slow', 'swing');
-    } else if
-        ($(this).scrollTop() <= 1) {
-        // длительность анимации - 'slow'
-        // тип анимации -  'linear'
-        $('.intro').slideDown('slow', 'linear');
-    }
-})
-// $(window).scroll(function () {
-//     if ($(this).scrollTop() >= 5) {
-//         // длительность анимации - 'slow'
-//         // тип анимации -  'linear'
-//         $('.intro').fadeIn('slow', 'linear');
-//     }
-//     else {
-//         // длительность анимации - 'fast'
-//         // тип анимации -  'swing'
-//         $('.intro').fadeOut('fast', 'swing');
-//     }
-// });
-
