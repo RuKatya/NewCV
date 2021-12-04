0fc1a1125f66ad8c76aa2d83d4953bb49591ce08
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2021-12-04 20:32:34 +0200

    lang

--- public/script/index.js
+++ public/script/index.js
@@ -6,3 +6,30 @@ const button = document.getElementById('menu');
             const menu = document.getElementById('navbar__menu');
             menu.classList.toggle('isopen')
         })
+
+const select = document.querySelector("select")
+const allLang = ["en","he","ru"]
+ 
+select.addEventListener("change", changeURLlang)
+
+function changeURLlang() {
+    let lang = select.value;
+
+    location.href = `${window.location.pathname}#${lang}`;
+
+    location.reload();
+}
+
+function changelang () {
+    let hash = window.location.hash;
+    hash = hash.substr(1) //remove the # symbole
+    console.log(hash)
+    if(!allLang.includes(hash)) {
+        location.href = `${window.location.pathname}#en`;
+        location.reload();
+    }
+
+    select.value = hash;
+}
+
+changelang()
---
+++ public/script/lang.js
@@ -0,0 +1,191 @@
+const landArr = {
+    //NAVBAR
+    "navbar": {
+        "works": {
+            "en": "My works",
+            "he": "העבודות שלי",
+            "ru": "Мои работы"
+        },
+        "skills": {
+            "en": "Skills",
+            "he": "",
+            "ru": ""
+        },
+        "contact": {
+            "en": "Contacts",
+            "he": "",
+            "ru": ""
+        },
+        "certificat": {
+            "en": "Certificat",
+            "he": "",
+            "ru": ""
+        },
+        "dowload": {
+            "en": "Download CV",
+            "he": "",
+            "ru": ""
+        },
+        "aboutme": {
+            "button": {
+                "en": "More about me",
+                "he": "",
+                "ru": ""
+            },
+            "goodday": {
+                "en": "Good day,",
+                "he": "",
+                "ru": ""
+            },
+            "aboutme": {
+                "info": {
+                    "p1": "All my life I was confident that I wanted to work in a medical field. I thought about studying physiotherapy or nursing. I decided to study medical systems management and afterwards continue on to nursing. It took time to understand that taking care of patients is too much responsibility for me, whilst studying for my degree programing courses made much more sense and aroused my interest. Even though it was clear I wouldn't work in the medical sphere after graduating, I kept on studying until I finished my BA in order to have a profession in the future.",
+                    "p2": "After graduating, I undertook several courses from YouTube and paid courses in C, Java. While exploring different languages, I felt empowered, observing the code turning from nothing to something beneficial. It got me excited. Subsequently, Javascript encouraged me to become a web developer. A combination of CSS, HTML and more can provide a seamless experience to a user through the browser. Finally, I decided to turn it into a career; that’s why you are reading my CV."
+                },
+            },
+        }
+    },
+    //INTRO
+    "intro": {
+        "name": {
+            "en": "Katya Rukosuev",
+            "he": "",
+            "ru": ""
+        },
+        "descr": {
+            "en": "Fullstack Frontend developer",
+            "he": "",
+            "ru": ""
+        },
+    },
+    //CONTACTS
+    "contacts": {
+        "github": {
+            "en": "Github",
+            "he": "",
+            "ru": ""
+        },
+        "linkedin": {
+            "en": "Linkedin",
+            "he": "",
+            "ru": ""
+        },
+        "gmail": {
+            "en": "Gmail",
+            "he": "",
+            "ru": ""
+        },
+        "whatsapp": {
+            "en": "WhatsApp",
+            "he": "",
+            "ru": ""
+        },
+        "phone": {
+            "en": "Phone",
+            "he": "",
+            "ru": ""
+        },
+    },
+    //WORKS
+    "projects": {
+        //QUIZ
+        "quiz": {
+            "head": {
+                "en": "Quiz please",
+                "he": "",
+                "ru": ""
+            },
+            "build": {
+                "en": "React, MongoDB, NodeJs, Scss",
+                "he": "",
+                "ru": ""
+            },
+            "desc": {
+                "en": "This project is a trivia questionnaire with a time limit. The trivia contains 23 questions which are limited to 10 minute. The questionnaire can be answered alone or with friends.",
+                "he": "",
+                "ru": ""
+            },
+            "comein": {
+                "en": " Come on in, take a look and enjoy!",
+                "he": "",
+                "ru": ""
+            },
+        },
+        // BLOG
+        "blog": {
+            "head": {
+                "en": "Blog site",
+                "he": "",
+                "ru": ""
+            },
+            "build": {
+                "en": "Next js, Api, Scss",
+                "he": "",
+                "ru": ""
+            },
+            "desc": {
+                "en": "I built the project after self-learning the NextJS framework. After experimenting with NextJS during my studies, while our course built a website for the Viviross company, in the process I decided to strengthen my expertise further. On that site you can see blogs by different users and the comments that were posted. The data was taken from a free API.",
+                "he": "",
+                "ru": ""
+            },
+        },
+        // TASK
+        "task": {
+            "head": {
+                "en": "Task site",
+                "he": "",
+                "ru": ""
+            },
+            "build": {
+                "en": "Ejs, MongoDB, JS, NodeJs, Scss",
+                "he": "",
+                "ru": ""
+            },
+            "desc": {
+                "en": "This project was built independently of my studies with the knowledge learned from my courses as well as self-taught skills. The site is a pesonalised to do list app. You can register on the site with your email or use the existing email below for testing:",
+                "he": "",
+                "ru": ""
+            },
+            "email": {
+                "en": "E-mail:",
+                "he": "",
+                "ru": ""
+            },
+            "pass": {
+                "en": "Password:",
+                "he": "",
+                "ru": ""
+            },
+
+        },
+        "visit": {
+            "en": "Visit",
+            "he": "",
+            "ru": ""
+        },
+    },
+    //SKILLS
+    "skills":{
+        "en": "Skills",
+            "he": "",
+            "ru": ""
+    },
+    //FOOTER
+    "footer": {
+        "source": {
+            "en": "Contact",
+            "he": "",
+            "ru": ""
+        },
+        "created": {
+            "en": "Created by Katya Rukosuev",
+            "he": "",
+            "ru": ""
+        },
+        "contact": {
+            "en": "Contact",
+            "he": "",
+            "ru": ""
+        },
+    },
+}
