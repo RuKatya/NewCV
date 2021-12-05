a78e25daaaa5ec4045b050ffccdfc987563585be
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2021-12-05 18:12:45 +0200

    lang

--- public/script/index.js
+++ public/script/index.js
@@ -1,15 +1,16 @@
 const button = document.getElementById('menu');
 
-        button.addEventListener('click', function (event) {
-            event.preventDefault();
+button.addEventListener('click', function (event) {
+    event.preventDefault();
 
-            const menu = document.getElementById('navbar__menu');
-            menu.classList.toggle('isopen')
-        })
+    const menu = document.getElementById('navbar__menu');
+    menu.classList.toggle('isopen')
+})
 
+//CHANGE LANG
 const select = document.querySelector("select")
-const allLang = ["en","he","ru"]
- 
+const allLang = ["en", "he", "ru"]
+
 select.addEventListener("change", changeURLlang)
 
 function changeURLlang() {
@@ -20,16 +21,69 @@ function changeURLlang() {
     location.reload();
 }
 
-function changelang () {
+function changelang() {
     let hash = window.location.hash;
     hash = hash.substr(1) //remove the # symbole
     console.log(hash)
-    if(!allLang.includes(hash)) {
+    if (!allLang.includes(hash)) {
         location.href = `${window.location.pathname}#en`;
-        location.reload();
+        // location.reload();
+    }
+
+    if (hash == "he") {
+        const he = document.querySelector("body")
+        he.setAttribute("dir", "rtl")
     }
 
     select.value = hash;
+
+    // TITLE
+    document.querySelector("title").innerHTML = langArr["title"][hash];
+
+    //-----TRANSLATE ALL-----//
+    for (let key in langArr) {
+        let elem = document.querySelector(`.lng-${key}`)
+        let trs = langArr[key][hash]
+
+        if (elem && trs) {
+            elem.innerHTML = trs;
+        }
+    }
+
+    //------VISIT BTN------//
+    const visitBtn = document.querySelectorAll(".lng-visit")
+    for (const visit of visitBtn) {
+        visit.innerHTML = langArr["visit"][hash]
+    }
+
+    //-----SKILLS BTN-------//
+    const skills = document.querySelectorAll(".lng-skill")
+    for (const skill of skills) {
+        skill.innerHTML = langArr["skill"][hash]
+    }
+
+    //----CONTACTS----//
+    const contact = document.querySelectorAll(".lng-contact")
+    console.log(contact)
+    for (const cont of contact) {
+        cont.innerHTML = langArr["contact"][hash]
+    }
+
+    // //------HOBBIES----//
+    // const hobbies = document.querySelector(".tooltip")
+    // console.log(hobbies)
+    // hobbies.innerHTML = `<div class="tooltip lng-hoobies">${langArr["hoobies"][hash]} 
+    //                         <span class="tooltiptext">
+    //                             <ul>
+    //                                 <li class="lang-list-item1">${langArr["lang-list-item1"][hash]}</li>
+    //                                 <li class="lang-list-item2">${langArr["lang-list-item2"][hash]}</li>
+    //                                 <li class="lang-list-item3">${langArr["lang-list-item3"][hash]}</li>
+    //                                 <li class="lang-list-item4">${langArr["lang-list-item4"][hash]}</li>
+    //                                 <li class="lang-list-item5">${langArr["lang-list-item5"][hash]}</li>
+    //                                 <li class="lang-list-item6">${langArr["lang-list-item6"][hash]}</li>
+    //                             </ul>
+    //                         </span>
+    //                     </div>`
 }
 
 changelang()
--- public/script/lang.js
+++ public/script/lang.js
@@ -1,191 +1,217 @@
-const landArr = {
+const langArr = {
+    //TITLE
+    "title": {
+        "en": "Katya Ru | CV",
+        "he": 'קטיה רו | קו"ח',
+        "ru": "Катя Ру | Резюме"
+    },
     //NAVBAR
-    "navbar": {
-        "works": {
-            "en": "My works",
-            "he": "העבודות שלי",
-            "ru": "Мои работы"
-        },
-        "skills": {
-            "en": "Skills",
-            "he": "",
-            "ru": ""
-        },
-        "contact": {
-            "en": "Contacts",
-            "he": "",
-            "ru": ""
-        },
-        "certificat": {
-            "en": "Certificat",
-            "he": "",
-            "ru": ""
-        },
-        "dowload": {
-            "en": "Download CV",
-            "he": "",
-            "ru": ""
-        },
-        "aboutme": {
-            "button": {
-                "en": "More about me",
-                "he": "",
-                "ru": ""
-            },
-            "goodday": {
-                "en": "Good day,",
-                "he": "",
-                "ru": ""
-            },
-            "aboutme": {
-                "info": {
-                    "p1": "All my life I was confident that I wanted to work in a medical field. I thought about studying physiotherapy or nursing. I decided to study medical systems management and afterwards continue on to nursing. It took time to understand that taking care of patients is too much responsibility for me, whilst studying for my degree programing courses made much more sense and aroused my interest. Even though it was clear I wouldn't work in the medical sphere after graduating, I kept on studying until I finished my BA in order to have a profession in the future.",
-                    "p2": "After graduating, I undertook several courses from YouTube and paid courses in C, Java. While exploring different languages, I felt empowered, observing the code turning from nothing to something beneficial. It got me excited. Subsequently, Javascript encouraged me to become a web developer. A combination of CSS, HTML and more can provide a seamless experience to a user through the browser. Finally, I decided to turn it into a career; that’s why you are reading my CV."
-                },
-            },
-        }
+    "moreAboteMeBTN": {
+        "en": "More about me",
+        "he": "עוד עליי",
+        "ru": "Больше обо мне"
+    },
+    "download": {
+        "en": "Download CV",
+        "he": 'הורדת קו"ח',
+        "ru": "Скачать резюме"
+    },
+    "certificat": {
+        "en": "Certificat",
+        "he": "תעודה",
+        "ru": "Сертификат"
+    },
+    "works": {
+        "en": "My works",
+        "he": "העבודות שלי",
+        "ru": "Мои работы"
+    },
+    "skill": {
+        "en": "Skills",
+        "he": "מיומנויות",
+        "ru": "Навыки"
+    },
+    "contact": {
+        "en": "Contact",
+        "he": "יצירת קשר",
+        "ru": "Контакт"
     },
     //INTRO
-    "intro": {
-        "name": {
-            "en": "Katya Rukosuev",
-            "he": "",
-            "ru": ""
-        },
-        "descr": {
-            "en": "Fullstack Frontend developer",
-            "he": "",
-            "ru": ""
-        },
+    "introName": {
+        "en": "Katya Rukosuev",
+        "he": "קטיה רוקוסוייב",
+        "ru": "Катя Рукосуева"
     },
-    //CONTACTS
-    "contacts": {
-        "github": {
-            "en": "Github",
-            "he": "",
-            "ru": ""
-        },
-        "linkedin": {
-            "en": "Linkedin",
-            "he": "",
-            "ru": ""
-        },
-        "gmail": {
-            "en": "Gmail",
-            "he": "",
-            "ru": ""
-        },
-        "whatsapp": {
-            "en": "WhatsApp",
-            "he": "",
-            "ru": ""
-        },
-        "phone": {
-            "en": "Phone",
-            "he": "",
-            "ru": ""
-        },
+    "introDescr": {
+        "en": "Fullstack Frontend developer",
     },
     //WORKS
-    "projects": {
-        //QUIZ
-        "quiz": {
-            "head": {
-                "en": "Quiz please",
-                "he": "",
-                "ru": ""
-            },
-            "build": {
-                "en": "React, MongoDB, NodeJs, Scss",
-                "he": "",
-                "ru": ""
-            },
-            "desc": {
-                "en": "This project is a trivia questionnaire with a time limit. The trivia contains 23 questions which are limited to 10 minute. The questionnaire can be answered alone or with friends.",
-                "he": "",
-                "ru": ""
-            },
-            "comein": {
-                "en": " Come on in, take a look and enjoy!",
-                "he": "",
-                "ru": ""
-            },
-        },
-        // BLOG
-        "blog": {
-            "head": {
-                "en": "Blog site",
-                "he": "",
-                "ru": ""
-            },
-            "build": {
-                "en": "Next js, Api, Scss",
-                "he": "",
-                "ru": ""
-            },
-            "desc": {
-                "en": "I built the project after self-learning the NextJS framework. After experimenting with NextJS during my studies, while our course built a website for the Viviross company, in the process I decided to strengthen my expertise further. On that site you can see blogs by different users and the comments that were posted. The data was taken from a free API.",
-                "he": "",
-                "ru": ""
-            },
-        },
-        // TASK
-        "task": {
-            "head": {
-                "en": "Task site",
-                "he": "",
-                "ru": ""
-            },
-            "build": {
-                "en": "Ejs, MongoDB, JS, NodeJs, Scss",
-                "he": "",
-                "ru": ""
-            },
-            "desc": {
-                "en": "This project was built independently of my studies with the knowledge learned from my courses as well as self-taught skills. The site is a pesonalised to do list app. You can register on the site with your email or use the existing email below for testing:",
-                "he": "",
-                "ru": ""
-            },
-            "email": {
-                "en": "E-mail:",
-                "he": "",
-                "ru": ""
-            },
-            "pass": {
-                "en": "Password:",
-                "he": "",
-                "ru": ""
-            },
-
-        },
-        "visit": {
-            "en": "Visit",
-            "he": "",
-            "ru": ""
-        },
+    /* QUIZ*/
+    "quizHead": {
+        "en": "Quiz please",
+        "he": "קוויז, פליז",
+        "ru": "Квиз, плиз"
     },
-    //SKILLS
-    "skills":{
-        "en": "Skills",
-            "he": "",
-            "ru": ""
+    "quizBuild": {
+        "en": "React, MongoDB, NodeJs, Scss",
+    },
+    "quizDesc": {
+        "en": "This project is a trivia questionnaire with a time limit. The trivia contains 23 questions which are limited to 10 minute. The questionnaire can be answered alone or with friends.",
+        "he": "פרויקט זה הוא שאלון טריוויה עם הגבלת זמן. הטריוויה מכילה 23 שאלות המוגבלות ל-10 דקות. ניתן לענות על השאלון לבד או עם חברים.",
+        "ru": "Этот проект представляет собой анкету по пустякам с ограничением по времени. Общая информация содержит 23 вопроса, которые ограничены 10 минутами. На анкету можно ответить в одиночку или с друзьями. "
+    },
+    "quizComeIn": {
+        "en": " Come on in, take a look and enjoy!",
+        "he": "היכנסו, תסתכלו ותיהנו!",
+        "ru": "Заходите, смотрите и наслаждайтесь!"
+    },
+    /* BLOG*/
+    "blogHead": {
+        "en": "Blog",
+        "he": "בלוג",
+        "ru": "Блог"
+    },
+    "blogBuild": {
+        "en": "Next js, Api, Scss",
+    },
+    "blogDesc": {
+        "en": "I built the project after self-learning the NextJS framework. After experimenting with NextJS during my studies, while our course built a website for the Viviross company, in the process I decided to strengthen my expertise further. On that site you can see blogs by different users and the comments that were posted. The data was taken from a free API.",
+        "he": "בניתי את הפרויקט לאחר למידה עצמית של מסגרת NextJS. לאחר התנסות ב-NextJS במהלך הלימודים, בזמן שהקורס שלנו בנה אתר לחברת Viviross, תוך כדי כך החלטתי לחזק את המומחיות שלי עוד יותר. באתר זה תוכלו לראות בלוגים של משתמשים שונים ואת ההערות שפורסמו. הנתונים נלקחו מ-API חינמי.",
+        "ru": "Я построил проект после самообучения фреймворка NextJS. После экспериментов с NextJS во время учебы, в то время как наш курс создавал веб-сайт для компании Viviross, в процессе я решил еще больше укрепить свой опыт. На этом сайте вы можете увидеть блоги разных пользователей и опубликованные комментарии. Данные были взяты из бесплатного API."
+    },
+    /* TASK */
+    "taskHead": {
+        "en": "Task site",
+        "he": "אתר משימות",
+        "ru": "Сайт задачь"
+    },
+    "taskBuild": {
+        "en": "Ejs, MongoDB, JS, NodeJs, Scss",
+    },
+    "taskDesc": {
+        "en": "This project was built independently of my studies with the knowledge learned from my courses as well as self-taught skills. The site is a pesonalised to do list app. You can register on the site with your email or use the existing email below for testing:",
+        "he": "פרויקט זה נבנה ללא תלות בלימודים שלי עם הידע שנלמד מהקורסים שלי כמו גם מיומנויות אוטודידקטיות. האתר הוא אפליקציית רשימת מטלות בהתאמה אישית. אתה יכול להירשם לאתר עם המייל שלך או להשתמש במייל הקיים למטה לצורך בדיקה",
+        "ru": "Этот проект был построен независимо от моей учебы с использованием знаний, полученных на моих курсах, а также навыков самоучки. Сайт представляет собой персонализированное приложение со списком дел. Вы можете зарегистрироваться на сайте, указав свой адрес электронной почты, или использовать существующий адрес электронной почты, указанный ниже, для тестирования."
+    },
+    "taskEmail": {
+        "en": "E-mail:",
+        "he": "איי-מייל:",
+        "ru": "E-мейл"
+    },
+    "taskPass": {
+        "en": "Password:",
+        "he": "סיסמא:",
+        "ru": "Пароль:"
+    },
+    "visit": {
+        "en": "Visit",
+        "he": "צפיה",
+        "ru": "Посетить"
+    },
+    //CONTACTS
+    "phone": {
+        "en": "Phone",
+        "he": "טלפון",
+        "ru": "Телефон"
     },
     //FOOTER
-    "footer": {
-        "source": {
-            "en": "Contact",
-            "he": "",
-            "ru": ""
-        },
-        "created": {
-            "en": "Created by Katya Rukosuev",
-            "he": "",
-            "ru": ""
-        },
-        "contact": {
-            "en": "Contact",
-            "he": "",
-            "ru": ""
-        },
+    "source": {
+        "en": "Code source",
+        "he": "מקור קוד",
+        "ru": "Исходный код"
     },
+    "created": {
+        "en": "Created by Katya Rukosuev",
+        "he": 'נוצר ע"י קטיה רוקוסוייב',
+        "ru": "Создано Катей Рукосуевой"
+    },
+
+    // ABOUT ME PAGE
+    "goodday": {
+        "en": "Good day,",
+        "he": "יום טוב, ",
+        "ru": "Хорошего дня, "
+    },
+    "p1": {
+        "en": "All my life I was confident that I wanted to work in a medical field. I thought about studying physiotherapy or nursing. I decided to study medical systems management and afterwards continue on to nursing. It took time to understand that taking care of patients is too much responsibility for me, whilst studying for my degree programing courses made much more sense and aroused my interest. Even though it was clear I wouldn't work in the medical sphere after graduating, I kept on studying until I finished my BA in order to have a profession in the future.",
+        "he": "כל חיי הייתי בטוח שאני רוצה לעבוד בתחום רפואי. חשבתי ללמוד פיזיותרפיה או סיעוד. החלטתי ללמוד ניהול מערכות רפואיות ולאחר מכן להמשיך לסיעוד. לקח זמן להבין שטיפול בחולים הוא אחריות גדולה מדי עבורי, בעוד שהלימוד בקורסי התכנות שלי היה הרבה יותר הגיוני ועורר בי עניין. למרות שהיה ברור שלא אעבוד בתחום הרפואי לאחר סיום הלימודים, המשכתי ללמוד עד שסיימתי את התואר הראשון כדי שיהיה לי מקצוע בעתיד.",
+        "ru": "Всю жизнь я был уверен, что хочу работать в сфере медицины. Я думал об изучении физиотерапии или медсестре. Я решила изучить управление медицинскими системами, а затем перейти к сестринскому делу. Потребовалось время, чтобы понять, что забота о пациентах - это слишком большая ответственность для меня, в то время как обучение на курсах программирования на получение степени имело гораздо больше смысла и пробудило мой интерес. Несмотря на то, что было ясно, что я не буду работать в сфере медицины после окончания учебы, я продолжал учиться, пока не получил степень бакалавра, чтобы иметь профессию в будущем."
+    },
+    "p2": {
+        "en": "After graduating, I undertook several courses from YouTube and paid courses in C, Java. While exploring different languages, I felt empowered, observing the code turning from nothing to something beneficial. It got me excited. Subsequently, Javascript encouraged me to become a web developer. A combination of CSS, HTML and more can provide a seamless experience to a user through the browser. Finally, I decided to turn it into a career; that’s why you are reading my CV.",
+        "he": "לאחר סיום הלימודים, עברתי מספר קורסים מיוטיוב וקורסים בתשלום ב-C, Java. תוך כדי חקר שפות שונות, הרגשתי מועצמת, התבוננתי בקוד שהופך מכלום למשהו מועיל. זה גרם לי להתרגש. לאחר מכן, Javascript עודד אותי להפוך למפתח אינטרנט. שילוב של CSS, HTML ועוד יכול לספק חוויה חלקה למשתמש דרך הדפדפן. לבסוף, החלטתי להפוך את זה לקריירה; זו הסיבה שאתה קורא את קורות החיים שלי.",
+        "ru": "После окончания я прошел несколько курсов на YouTube и платные курсы по C, Java. Изучая разные языки, я чувствовал себя уполномоченным, наблюдая, как код превращается из ничего во что-то полезное. Это меня взволновало. Впоследствии Javascript побудил меня стать веб-разработчиком. Комбинация CSS, HTML и др. Может обеспечить удобство работы пользователя через браузер. В конце концов, я решил превратить это в карьеру; поэтому вы читаете мое резюме."
+    },
+    /* HOBBIES*/
+    "hoobies": {
+        "en": "Hobbies",
+        "he": "תחביבים",
+        "ru": "Хобби"
+    },
+    "list-item1": {
+        "en": "3D modeling, puzzles",
+        "he": "",
+        "ru": ""
+    },
+    "list-item2": {
+        "en": "Programing",
+        "he": "",
+        "ru": ""
+    },
+    "list-item3": {
+        "en": "Puzzle solving games",
+        "he": "",
+        "ru": ""
+    },
+    "list-item4": {
+        "en": "Traveling abroad",
+        "he": "",
+        "ru": ""
+    },
+    "list-item5": {
+        "en": "Reading books",
+        "he": "",
+        "ru": ""
+    },
+    "list-item6": {
+        "en": "Grow plants",
+        "he": "",
+        "ru": ""
+    },
+
+
 }
+
+
+
+
+
+
+
+
+
+
+    // "navbar": {
+
+
+
+
+    //     "aboutme": {
+    //         "button": {
+    //             "en": "More about me",
+    //                 "he": "",
+    //                     "ru": ""
+    //         },
+    //         "goodday": {
+    //             "en": "Good day,",
+    //                 "he": "",
+    //                     "ru": ""
+    //         },
+    //         "aboutme": {
+    //             "info": {
+    //                 "p1": "All my life I was confident that I wanted to work in a medical field. I thought about studying physiotherapy or nursing. I decided to study medical systems management and afterwards continue on to nursing. It took time to understand that taking care of patients is too much responsibility for me, whilst studying for my degree programing courses made much more sense and aroused my interest. Even though it was clear I wouldn't work in the medical sphere after graduating, I kept on studying until I finished my BA in order to have a profession in the future.",
+    //                     "p2": "After graduating, I undertook several courses from YouTube and paid courses in C, Java. While exploring different languages, I felt empowered, observing the code turning from nothing to something beneficial. It got me excited. Subsequently, Javascript encouraged me to become a web developer. A combination of CSS, HTML and more can provide a seamless experience to a user through the browser. Finally, I decided to turn it into a career; that’s why you are reading my CV."
+    //             },
+    //         },
+    //     }
+    // },
