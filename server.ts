9f7396eb93e0b93e258685c5cdbd35be8b95527f
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-05-10 15:29:22 +0300

    mongoose

--- server.ts
+++ server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import { connectDB } from './connectDB';
 const app = express(); //express
+
 const PORT = process.env.PORT ?? 5457; //connect to port 5457
 require('dotenv').config()
 
+app.use(express.static('my-cv/build')) //static
+
+connectDB()
 
 try {
     app.listen(PORT, () => {
