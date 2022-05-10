68652d386636b83c5bcf133ed0561b610f18259e
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-05-10 14:52:10 +0300

    json

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-//EXPRESS
 const express = require('express');
 const app = express();
-//PATH
 const path = require('path')
-//PORT
 const PORT = process.env.PORT ?? 7171; //connect to port 7171
-//COLORS
 const color = require('colors')
-
-//EJS
-app.set('view engine', 'ejs') //connecting ejs
-console.log(app.get('view engine'))
-app.set('views', path.resolve(__dirname, 'pages'))
 
 app.use(express.static(path.resolve(__dirname, 'public'))) //static
 
@@ -20,7 +11,6 @@ const index = require('./router/index')
 
 app.use('/', index)
 
-//CONNECT TO PORT
 try {
     app.listen(PORT, () => {
         console.log(color.bgBlue.black(`Server listen on`, color.bgBlue.white(`http://localhost:${PORT}`)))
