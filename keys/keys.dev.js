979cb28098a827fe1b54555a5dd83daf9ecff4b4
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-05-17 15:08:35 +0300

    Create keys.dev.js

---
+++ keys/keys.dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGO_URL: 'mongodb+srv://KatyaRu:QPDSGxTEiwtFMO5P@cluster0.f6pgs.mongodb.net/mycv',
+    SECRET: 'what-what-in-the-but',
+    ROLE: 'i-can-change-every-thing'
+};
